d9e51c489b94bd164af96b9109e6f4afce373dda
zongshun <zhangzs@bu.edu>
2024-04-12 05:15:13 +0000

    prediction component private registry

--- prediction_XGBoost_openshift_image/build.sh
+++ prediction_XGBoost_openshift_image/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 image_name=zongshun96/prediction_xgb_openshift
-image_tag=0.01
+image_tag=1.0
 full_image_name=${image_name}:${image_tag}
 
 # cd "$(dirname "$0")" 
-sudo docker build -t "${full_image_name}" .
-sudo docker push "$full_image_name"
+docker build -t "registry-route-ai4cloudops-11855c.apps.shift.nerc.mghpcc.org/${full_image_name}" .
+docker push "registry-route-ai4cloudops-11855c.apps.shift.nerc.mghpcc.org/$full_image_name"
 
 # Output the strict image name, which contains the sha256 image digest
 #docker inspect --format="{{index .RepoDigests 0}}" "${full_image_name}"
