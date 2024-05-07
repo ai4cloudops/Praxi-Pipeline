9e9916e8dcbe46f54fdc0f7d858eb7b0d4ae00e2
zongshun <zhangzs@bu.edu>
2024-05-07 22:30:00 +0000

    doc versions of sparse matrix implementation

--- prediction_XGBoost_openshift_image/build.sh
+++ prediction_XGBoost_openshift_image/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 image_name=zongshun96/prediction_xgb_openshift
-image_tag=1.0
+image_tag=1.2
 full_image_name=${image_name}:${image_tag}
 
 # cd "$(dirname "$0")" 
@@ -9,3 +9,9 @@ docker push "registry-route-ai4cloudops-11855c.apps.shift.nerc.mghpcc.org/$full_
 
 # Output the strict image name, which contains the sha256 image digest
 #docker inspect --format="{{index .RepoDigests 0}}" "${full_image_name}"
+
+
+
+# 1.0 csr
+# 1.1 coo
+# 1.2 coo without selection
