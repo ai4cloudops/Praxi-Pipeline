9b19f6cc555f6d773989b826801b6dd767c9b430
zongshun <zhangzs@bu.edu>
2025-04-16 19:50:23 +0000

    Generate dockerfiles using a fixed lisst of packages with versions.

--- gen_data_docker_image/python/templating_image/image_templating.py
+++ gen_data_docker_image/python/templating_image/image_templating.py
@@ -1,3 +1,4 @@
+import pickle
 from string import Template
 from itertools import combinations
 import collections
@@ -40,7 +41,7 @@ def fetch_versions_for_multiple_packages(package_names):
 
 def load_package_rank(filter_l=set(),count=1000):
     import json
-    with open('/home/cc/Praxi-study/Praxi-Pipeline/gen_data_docker_image/python/templating_image/top-pypi-packages-30-days.min.json') as f:
+    with open('/home/cc/Praxi-Pipeline/gen_data_docker_image/python/templating_image/top-pypi-packages-30-days.min.json') as f:
         d = json.load(f)
         project_l = set()
         for idx, row in enumerate(d["rows"]):
@@ -78,7 +79,7 @@ def gen_dockerfile(all_dep, choose=1, base_images=["python:3.9.18-bullseye", "py
             }
 
             # Load the template
-            with open('/home/cc/Praxi-study/Praxi-Pipeline/gen_data_docker_image/python/templating_image/DockerfileTemplate.txt', 'r') as file:
+            with open('/home/cc/Praxi-Pipeline/gen_data_docker_image/python/templating_image/DockerfileTemplate.txt', 'r') as file:
                 template = Template(file.read())
 
             # Substitute placeholders with actual values
@@ -86,7 +87,7 @@ def gen_dockerfile(all_dep, choose=1, base_images=["python:3.9.18-bullseye", "py
 
             # Save the rendered Dockerfile
             dependencies_str = (base_image.split("/")[-1].replace(":","")).replace(".","_")+'.'+("_p_".join([dep.replace("==", "_v") for dep in dependencies])).replace(".","_")
-            save_path = '/home/cc/Praxi-study/Praxi-Pipeline/gen_data_docker_image/python/dockerfiles/Dockerfile.'+dependencies_str
+            save_path = '/home/cc/Praxi-Pipeline/gen_data_docker_image/python/dockerfiles/Dockerfile.'+dependencies_str
             with open(save_path, 'w') as file:
                 file.write(dockerfile_content)
             # if save_path in seen:
@@ -109,7 +110,7 @@ def format_dep_with_versions(package_versions, filter_l=set()):
                     break
         if count < 3:
             print("count:", count,package_name, versions)
-            Path('/home/cc/Praxi-study/Praxi-Pipeline/gen_data_docker_image/python/dockerfiles_failed/'+'Dockerfile..'+package_name).touch()
+            Path('/home/cc/Praxi-Pipeline/gen_data_docker_image/python/dockerfiles_failed/'+'Dockerfile..'+package_name).touch()
     # print(all_dep_with_ver)
     return all_dep_with_ver
 
@@ -119,31 +120,43 @@ def find_dockerfiles(directory):
     return list(path.glob('Dockerfile.*'))
 
 if __name__ == "__main__":
-    dockerfiles_failed_dir = "/home/cc/Praxi-study/Praxi-Pipeline/gen_data_docker_image/python/dockerfiles_failed"
-    if not Path(dockerfiles_failed_dir).exists():
-        Path(dockerfiles_failed_dir).mkdir()
-    dockerfiles = find_dockerfiles(dockerfiles_failed_dir)
-    # print([dockerfile.name for dockerfile in dockerfiles])
+    # dockerfiles_failed_dir = "/home/cc/Praxi-Pipeline/gen_data_docker_image/python/dockerfiles_failed"
+    # if not Path(dockerfiles_failed_dir).exists():
+    #     Path(dockerfiles_failed_dir).mkdir()
+    # dockerfiles = find_dockerfiles(dockerfiles_failed_dir)
+    # # print([dockerfile.name for dockerfile in dockerfiles])
 
-    dockerfiles_dir = "/home/cc/Praxi-study/Praxi-Pipeline/gen_data_docker_image/python/dockerfiles"
+    dockerfiles_dir = "/home/cc/Praxi-Pipeline/gen_data_docker_image/python/dockerfiles"
     if not Path(dockerfiles_dir).exists():
         Path(dockerfiles_dir).mkdir()
 
-    filter_l = set([dockerfile.name.split(".")[-1] for dockerfile in dockerfiles])
-    # print(filter_l)
+    # filter_l = set([dockerfile.name.split(".")[-1] for dockerfile in dockerfiles])
+    # # print(filter_l)
 
-    all_dep = load_package_rank(filter_l)
-    # print(all_dep)
+    # all_dep = load_package_rank(filter_l)
+    # # print(all_dep)
 
-    package_versions = fetch_versions_for_multiple_packages(all_dep)
-    # # Print the versions
-    # for package_name, versions in package_versions.items():
-    #     print(f"{package_name}: {versions}")
+    # package_versions = fetch_versions_for_multiple_packages(all_dep)
+    # # # Print the versions
+    # # for package_name, versions in package_versions.items():
+    # #     print(f"{package_name}: {versions}")
 
-    all_dep_with_ver = format_dep_with_versions(package_versions, filter_l)
+    # all_dep_with_ver = format_dep_with_versions(package_versions, filter_l)
+
+    index_label_mapping_path = "/home/cc/Praxi-Pipeline/data/data4/index_label_mapping"
+    # with open(index_label_mapping_path, 'wb') as fp:
+    #     all_label_l = pickle.load(fp)
+    with open(index_label_mapping_path, 'rb') as fp:
+        all_dep_with_ver = set(pickle.load(fp))
 
     # saved = gen_dockerfile(list(all_dep_with_ver), choose=1, base_images=["public.ecr.aws/docker/library/python:3.9.18-bullseye", "public.ecr.aws/docker/library/python:3.9-slim-bullseye", "public.ecr.aws/docker/library/python:3.9-slim-bookworm", "public.ecr.aws/docker/library/python:3.9.18-bookworm"])
-    saved = gen_dockerfile(list(all_dep_with_ver), choose=1)
+    saved = gen_dockerfile(list(all_dep_with_ver), choose=2)
+
+    # index_label_mapping_path = "/home/cc/Praxi-Pipeline/data/data4/index_label_mapping"
+    # # with open(index_label_mapping_path, 'wb') as fp:
+    # #     all_label_l = pickle.load(fp)
+    # with open(index_label_mapping_path, 'rb') as fp:
+    #     clf_labels_l = pickle.load(fp)
 
     with open(f"{dockerfiles_dir}/inventory.json", "w") as outfile:
         json.dump(saved, outfile)
